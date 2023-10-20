819d6b11e186014cffc102ab31e24151beaf8490
Liri3d <124776802+Liri3d@users.noreply.github.com>
2023-10-21 00:06:49 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 
 int main() {
     std::string name;
-    std::cout << "Enter your name: "; // Prompt for name
+    std::cout << "Enter your name: "; // Запросить имя
     std::getline(std::cin, name);
-    std::cout << "Hello world from @" << name << std::endl; // Print greeting
+    std::cout << "Hello world from @" << name << std::endl; // Распечатать
     return 0;
 }
