c96e2839ed97e2b7c84d926fb66eec06768b406e
Liri3d <vlad.ogon.2004@gmail.com>
2023-10-20 22:57:26 +0300

    Добавлено приветствие по имени

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     string name;
     cout << "Enter your name: ";
-    cin >> name;
+    getline(cin, name);
     cout << "Hello world from @" << name << endl;
     return 0;
 }
