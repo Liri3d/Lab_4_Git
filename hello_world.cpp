e300117d001d425e22e42c489940cdcecfa29e4b
Liri3d <vlad.ogon.2004@gmail.com>
2023-10-20 23:21:54 +0300

    Добавлены комментарии к коду

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";
+    std::cout << "Enter your name: "; // Prompt for name
     std::getline(std::cin, name);
-    std::cout << "Hello world from @" << name << std::endl;
+    std::cout << "Hello world from @" << name << std::endl; // Print greeting
     return 0;
 }
