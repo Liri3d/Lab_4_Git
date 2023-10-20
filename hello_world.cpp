66cbc6d9f87771b8c3690bdfaafcea2af9da5fd9
Liri3d <vlad.ogon.2004@gmail.com>
2023-10-21 00:05:51 +0300

    Изменен стиль кода с использованием формата clang

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-int main() {
-    std::string name;
-    std::cout << "Enter your name: "; // Prompt for name
-    std::getline(std::cin, name);
-    std::cout << "Hello world from @" << name << std::endl; // Print greeting
-    return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: "; // Prompt for name
+  std::getline(std::cin, name);
+  std::cout << "Hello world from @" << name << std::endl; // Print greeting
+  return 0;
 }
